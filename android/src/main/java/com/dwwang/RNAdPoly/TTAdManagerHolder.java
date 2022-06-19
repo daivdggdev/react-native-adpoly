130fe2c2d9ab6a5cdc3772c6da4f3335234e8fe9
dwwang <daivdggdev@163.com>
2022-06-19 23:50:20 +0800

    fix: debug false

--- android/src/main/java/com/dwwang/RNAdPoly/TTAdManagerHolder.java
+++ android/src/main/java/com/dwwang/RNAdPoly/TTAdManagerHolder.java
@@ -30,7 +30,7 @@ public class TTAdManagerHolder {
                     .useTextureView(true) // 使用TextureView控件播放视频,默认为SurfaceView,当有SurfaceView冲突的场景，可以使用TextureView
                     .allowShowNotify(true) // 是否允许sdk展示通知栏提示
                     // .debug(BuildConfig.DEBUG) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
-                    .debug(true) // 测试阶段打开，可以通过日志排查问题，上线时去除该调用
+                    .debug(false) // 测试阶段打开，可以通过日志排查问题，上线时去除该调用
                     .directDownloadNetworkType(TTAdConstant.NETWORK_STATE_WIFI, TTAdConstant.NETWORK_STATE_4G) // 允许直接下载的网络状态集合
                     .supportMultiProcess(false)// 是否支持多进程
                     .needClearTaskReset()
