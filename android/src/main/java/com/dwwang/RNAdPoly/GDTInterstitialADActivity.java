c7ac6bf1223673bad056b2c620d9b49d515486a0
dwwang <daivdggdev@163.com>
2022-06-19 23:28:46 +0800

    feat: 删除无用的类型

--- android/src/main/java/com/dwwang/RNAdPoly/GDTInterstitialADActivity.java
+++ android/src/main/java/com/dwwang/RNAdPoly/GDTInterstitialADActivity.java
@@ -3,7 +3,6 @@ package com.dwwang.RNAdPoly;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
--- android/src/main/java/com/dwwang/RNAdPoly/GDTRewardVideoActivity.java
+++ android/src/main/java/com/dwwang/RNAdPoly/GDTRewardVideoActivity.java
@@ -3,7 +3,6 @@ package com.dwwang.RNAdPoly;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
